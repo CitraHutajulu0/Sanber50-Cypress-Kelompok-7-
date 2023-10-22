8be92a3cd921679a6411b35c1d500e2b26ff2d82
Habib An Najjar <habibannajjar20@gmail.com>
2023-10-22 21:41:57 +0700

    Create login.cy.js
    Belajar tes automation

---
+++ Quiz3-Kel-7/cypress/e2e/1-getting-started/login.cy.js
@@ -0,0 +1,13 @@
+describe('Login Test', () => {
+    it('Success Login', () => {
+        cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
+        // Di sini kita mengunjungi halaman login Magento dengan URL yang diberikan pada soal quis.
+        // Kemudian, kita dapat melanjutkan dengan tindakan yang sesuai pada halaman ini.
+        // Contoh lain, jika ingin memeriksa elemen berdasarkan teksnya:
+        cy.get('#email').type('habibannajjar20@gmail.com')
+        cy.get('#pass').type('Test12345')
+        cy.get('#send2').click()
+        cy.get('#page-title-wrapper').should('contain','My Account')
+    })
+
+})
