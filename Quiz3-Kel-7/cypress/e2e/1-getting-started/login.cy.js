b05bfd9fb6372ac1da74dc64e980dd49aba1e792
Habib An Najjar <habibannajjar20@gmail.com>
2023-10-22 23:54:42 +0700

    Update login.cy.js
    update Fail login

--- Quiz3-Kel-7/cypress/e2e/1-getting-started/login.cy.js
+++ Quiz3-Kel-7/cypress/e2e/1-getting-started/login.cy.js
@@ -1,6 +1,6 @@
 describe('Login Test', () => {
     it('Success Login', () => {
-        cy.visit('https://magento.softwaretestingboard.com/customer/account/login/')
+        cy.visit('/')
         // Di sini kita mengunjungi halaman login Magento dengan URL yang diberikan pada soal quis.
         // Kemudian, kita dapat melanjutkan dengan tindakan yang sesuai pada halaman ini.
         // Contoh lain, jika ingin memeriksa elemen berdasarkan teksnya:
@@ -8,6 +8,14 @@ describe('Login Test', () => {
         cy.get('#pass').type('Test12345')
         cy.get('#send2').click()
         cy.get('h1.page-title').should('contain', 'My Account')
+    })
+
+    it('Fail login', () => {
+        cy.visit('/')
+        cy.get('#email').type('habibannajjar@gmail.com')
+        cy.get('#pass').type('Test12345')
+        cy.get('#send2').click()
+        cy.get('div.block-customer-login').should('contain', 'Registered Customers')
     })
 
 })
