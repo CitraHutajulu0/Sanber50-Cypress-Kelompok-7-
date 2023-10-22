8bb2139d24a0ded8432fcc1287f9832296b6a1f0
MFebrianNGR <febrian0530@gmail.com>
2023-10-22 18:44:01 +0700

    Menambahkan Scenario di Process Checkout dan POM Process Checkout

--- Quiz3-Kel-7/cypress/e2e/1-getting-started/ProcessToCheckOut.cy.js
+++ Quiz3-Kel-7/cypress/e2e/1-getting-started/ProcessToCheckOut.cy.js
@@ -1,28 +1,46 @@
+import ProcessToCheckOut from "../../support/pageObject/ProcessToCheckOut"
+
 describe('Process to Checkout on Magento', () => {
-    it('performs the checkout process', () => {
-        cy.visit('https://magento.softwaretestingboard.com/') // Ganti URL dengan URL halaman utama situs Anda
-
-        // Misalnya kita ingin memilih produk dan menambahkannya ke keranjang.
-        cy.contains("Product Name").click() // Ganti dengan teks produk yang ingin kita pilih
-        cy.get('.tocart').click() // Misalkan ini adalah tombol untuk menambahkan ke keranjang
-
-        // Setelah menambahkan produk ke keranjang, Anda dapat melanjutkan ke halaman checkout.
-        cy.get('.action.primary.checkout').click() // Misalkan ini adalah tombol untuk checkout
-
-        // Selanjutnya, Anda bisa mengisi formulir informasi pengiriman dan metode pembayaran sesuai dengan kebutuhan.
-
-        // Contoh: Mengisi informasi pengiriman
-        cy.get('#firstname').type("John")
-        cy.get('#lastname').type("Doe")
-        cy.get('#street_1').type("123 Main St")
-        // Lanjutkan dengan mengisi elemen lainnya sesuai kebutuhan.
-
-        // Contoh: Pilih metode pembayaran
-        cy.get('#p_method_checkmo').click() // Misalkan ini adalah metode pembayaran yang dipilih
-
-        // Contoh: Lanjutkan ke halaman konfirmasi pesanan
-        cy.get('.primary').click() // Misalkan ini adalah tombol untuk melanjutkan
-
-        // Lakukan tindakan lain yang sesuai sesuai dengan skenario yang ingin Anda uji.
+    beforeEach(() => {
+        cy.visit('https://magento.softwaretestingboard.com/') //Access URL Magento
+    })
+    it.skipp('First time checkout Product', () => { //Note jika belum pernah melakukan checkout dan ada product di chart
+        ProcessToCheckOut.SignIn()
+        ProcessToCheckOut.InputEmail('citrahutajulu@gmail.com')
+        ProcessToCheckOut.InputPass('QA in my life')
+        ProcessToCheckOut.BtnLogin()
+        cy.wait(6000)
+        ProcessToCheckOut.BtnChart()
+        ProcessToCheckOut.BtnCheckout1()
+        cy.wait(6000)
+        ProcessToCheckOut.Fill_Comp('PT. Danamon Indonesia')
+        ProcessToCheckOut.Fill_Add1('Jl Sari Indah')
+        ProcessToCheckOut.Fill_Add2('Ciracas Segar')
+        ProcessToCheckOut.Fill_Add3('Ciracas')
+        ProcessToCheckOut.Fill_City('Jakarta Timur')
+        ProcessToCheckOut.Fill_Reg1('2')
+        ProcessToCheckOut.Fill_Post('663215-12345')
+        ProcessToCheckOut.Fill_Reg2('2')
+        ProcessToCheckOut.Fill_Phone('081345679876')
+        ProcessToCheckOut.Radio_Btn1()
+        ProcessToCheckOut.Next_Btn()
+        cy.wait(6000)
+        ProcessToCheckOut.Pay_Btn()
+        cy.contains('Thank you for your purchase!')
+    })
+    it('Have checked out before', () => { //Note jika sudah pernah melakukan checkout dan ada product di chart
+        ProcessToCheckOut.SignIn()
+        ProcessToCheckOut.InputEmail('citrahutajulu@gmail.com')
+        ProcessToCheckOut.InputPass('QA in my life')
+        ProcessToCheckOut.BtnLogin()
+        cy.wait(6000)
+        ProcessToCheckOut.BtnChart()
+        ProcessToCheckOut.BtnCheckout1()
+        cy.wait(10000)
+        ProcessToCheckOut.Radio_Btn1()
+        ProcessToCheckOut.Next_Btn()
+        cy.wait(10000)
+        ProcessToCheckOut.Pay_Btn()
+        cy.contains('Thank you for your purchase!')
     })
 })
