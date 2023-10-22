b3cc0aceb05a46d7864c44ba54af9c13fc5f14b3
ariadanangahmed <148179825+ariadanangahmed@users.noreply.github.com>
2023-10-22 09:59:07 +0700

    Update kunjungilogin.cy.js
    Menambah skenario fitur login
    
    Signed-off-by: ariadanangahmed <148179825+ariadanangahmed@users.noreply.github.com>

--- Quiz3-Kel-7/cypress/e2e/1-getting-started/kunjungilogin.cy.js
+++ Quiz3-Kel-7/cypress/e2e/1-getting-started/kunjungilogin.cy.js
@@ -7,3 +7,43 @@ describe('Access Login Page', () => {
         cy.contains("Sign In or Create an Account")
     })
 })
+
+describe('Tes Halaman Login', () => {
+  beforeEach(() => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/');
+  });
+
+  // Test scenario: Validasi judul halaman yang sesuai
+  it('Menampilkan judul halaman yang sesuai', () => {
+    cy.title().should('include', 'Customer Login');
+  });
+
+  // Test scenario: Validasi login dengan invalid email format
+  it('Memvalidasi skenario login dengan mengisi format email salah', () => {
+    cy.get('[name="login[username]"]').type('asdasdasdasdsadas');
+    cy.get('[name="login[password]"]').type('passwordinvalid');
+    cy.get('[name="send"]').click();
+    cy.get('#email-error').should('contain', 'Please enter a valid email address (Ex: johndoe@domain.com).');
+  });
+
+   // Test scenario: Validasi login dengan invalid credential
+  it('Memvalidasi skenario login dengan invalid credential', () => {
+    cy.get('[name="login[username]"]').type('invalidemail@gmail.com');
+    cy.get('[name="login[password]"]').type('invalidpassword');
+    cy.get('[name="send"]').click();
+    
+    cy.get('.message-error').should('have.length', 1);
+    cy.get('.message-error').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.');
+  });
+
+  // Test scenario: Validiasi skenario login dengan empty field
+  it('Memvalidasi skenari0 klik login dengan empty field', () => {
+    cy.get('[name="login[username]"]').clear();
+    cy.get('[name="login[password]"]').clear();
+    cy.get('[name="send"]').click();
+    
+    cy.get('.message-error').should('have.length', 1);
+    cy.get('.message-error').should('contain', 'A login and a password are required.');
+  });
+
+});
