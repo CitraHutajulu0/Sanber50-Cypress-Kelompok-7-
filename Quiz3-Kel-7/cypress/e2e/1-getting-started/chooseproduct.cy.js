992eddd637ebac213b1a8128328ce183381be230
Habibannajjar20 <56580695+Habibannajjar20@users.noreply.github.com>
2023-10-23 12:24:37 +0700

    Update chooseproduct.cy.js
    Edit 
    
    Signed-off-by: Habibannajjar20 <56580695+Habibannajjar20@users.noreply.github.com>

--- Quiz3-Kel-7/cypress/e2e/1-getting-started/chooseproduct.cy.js
+++ Quiz3-Kel-7/cypress/e2e/1-getting-started/chooseproduct.cy.js
@@ -1,18 +1,18 @@
 describe('Choose Product on Magento', () => {
-    it('selects a product', () => {
-        cy.visit('https://magento.softwaretestingboard.com/') // Ganti URL dengan URL halaman utama situs URL pada quiz 3
-describe('Pilih Produk', () => {
-    it('Tambah Produk', () => {
-      cy.visit('https://magento.softwaretestingboard.com/')
+  it('selects a product', () => {
+      cy.visit('https://magento.softwaretestingboard.com/') // Ganti URL dengan URL halaman utama situs URL pada quiz 3
 
-        // Misalkan Anda ingin mengklik pada produk dengan teks tertentu, seperti "Product Name".
-        // Anda perlu mengidentifikasi elemen produk dan kemudian mengkliknya.
-        cy.contains("Product Name").click()
+      // Misalkan Anda ingin mengklik pada produk dengan teks tertentu, seperti "Product Name".
+      // Anda perlu mengidentifikasi elemen produk dan kemudian mengkliknya.
+      cy.contains("Women").click()
 
         // Setelah mengklik produk, Anda dapat melanjutkan dengan tindakan lain seperti menambahkannya ke keranjang, dll.
-      cy.contains('Women').click()
       cy.contains('Radiant Tee').click()
       cy.contains('XS').click()
       cy.get('#option-label-color-93-item-56').click()
       cy.get('#product-addtocart-button').click()
       cy.get('#maincontent').should('contain', 'You added Radiant Tee to your')
+      cy.get('div.minicart-wrapper').click()
+
+  })
+})
